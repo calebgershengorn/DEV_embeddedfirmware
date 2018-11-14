b163b5c40bc81a6a68dc7fe63ee2db023c735ec5
Gerry Chen <gdc9@duke.edu>
2018-11-13 23:06:50 -0500

    nRF module

---
+++ IsolatedHardwareTests/nRF/nRF.ino
@@ -0,0 +1,55 @@
+#include "RF24.h"
+#include <Metro.h>
+
+RF24 radio(7,8);
+
+#define ROLE 0
+
+byte address[6] = "0node";
+
+Metro writeTimer(50);
+byte data;
+
+bool writeRadio(byte data);
+void readRadio();
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println(F("DEV radio"));
+  Serial.print(F("Initializing..."));
+  
+  radio.begin();
+  radio.setPALevel(RF24_PA_MAX);
+  radio.enableDynamicAck();
+  radio.openWritingPipe(address);
+  radio.openReadingPipe(1, address);
+  radio.printDetails();
+  radio.startListening();
+
+  Serial.println(F(" complete"));
+}
+
+void loop() {
+  #if ROLE==1
+    if (writeTimer.check()){
+      writeRadio(millis());
+    }
+  #endif
+  
+  readRadio();
+}
+
+bool writeRadio(byte data) {
+  radio.stopListening();
+  bool success = radio.write(&data, sizeof(byte));
+  radio.startListening();
+  return success;
+}
+void readRadio() {
+  while (radio.available()) {
+    radio.read(&data, sizeof(byte));
+    Serial.print("Got ");
+    Serial.println(data);
+  }
+}
+
